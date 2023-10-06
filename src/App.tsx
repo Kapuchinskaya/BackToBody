907b9b71756fe5a7b71dd40b123ab445cca0b7ad
Volha Kuchynskaya <kapuchinskaya.study@gmail.com>
2023-10-06 15:38:11 +0200

    HomePage (part1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import HomePage from "./components/HomePage";
 
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <HomePage />
+    </>
+  );
 };
 
 export default App;
